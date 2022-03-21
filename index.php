bd7df6553a7c743adaa8918328479b78ad7a1b2a
Bencio09 <lorebencini13@outlook.com>
2022-03-21 18:25:37 +0100

    GET implementato

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
 <?php
-    $request = $_SERVER["REQUEST_METHOD"];
-    echo($request);
-    file_get_contents("data.json")
-    header("")
+    $method = $_SERVER["REQUEST_METHOD"];
+    $request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
+    $fileJSON = file_get_contents("data.json");
+    header("Content-Type: application/json");
+
+    switch($fileJSON){
+        case 'GET':
+            var_dump($fileJSON);
+            header("HTTP/1.1 200 OK");
+            break;
+        case 'POST':
+            break;
+    
+        case 'PUT':
+            break;
+    
+        case 'DELETE':
+            break;
+        default:
+            header("HTTP/1.1 400 BAD REQUEST");
+            break;
+    }
 ?>
