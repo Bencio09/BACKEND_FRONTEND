ec9b436d14a748cb4c55a36350ee7c854c203d70
lai2-01 <lai2-01@itismeucci.com>
2022-03-28 11:44:25 +0200

    Commit

--- index.php
+++ index.php
@@ -2,26 +2,29 @@
     $method = $_SERVER["REQUEST_METHOD"];
     $request = explode("/", substr(@$_SERVER['PATH_INFO'], 1));
     $fileJSON = file_get_contents("data.json");
+    //echo $fileJSON;
+    $data = json_decode($fileJSON, true);
+    
     header("Content-Type: application/json");
-
-    switch($fileJSON){
+    switch($method){
         case 'GET':
             echo "GET";
-            header("HTTP/1.1 200 OK");
+            echo $data[19];
+            //echo var_dump($fileJSON);
             break;
         case 'POST':
             echo "POST";
-            header("HTTP/1.1 200 OK");
+            for ($i=0; $i < 20; $i++) { 
+                echo $data["_embedded"]["employees"][$i];
+            }
             break;
     
         case 'PUT':
             echo "PUT";
-            header("HTTP/1.1 200 OK");
             break;
     
         case 'DELETE':
             echo "DELETE";
-            header("HTTP/1.1 200 OK");
             break;
         default:
             header("HTTP/1.1 400 BAD REQUEST");
