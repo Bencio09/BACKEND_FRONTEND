0c2f7caff4875648c22a1792fe68a823ea8841cd
lai2-01 <lai2-01@itismeucci.com>
2022-04-06 13:46:52 +0200

    òlkkcvhljxz

--- connesssione.php
+++ connesssione.php
@@ -1,7 +1,7 @@
 <?php
 
-$host = "172.17.0.1:3306";
-$user = "root";
-$password = "root";
-$db = "mydb";
+    $host = "172.17.0.1:3306";
+    $user = "root";
+    $password = "root";
+    $db = "mydb";
 ?>
