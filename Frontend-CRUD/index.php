b15b118b30ef29852bb16df96965b14ab7a836c2
Bencio09 <lorebencini13@outlook.com>
2022-05-24 20:13:58 +0200

    Pensato get specifico

--- Frontend-CRUD/index.php
+++ Frontend-CRUD/index.php
@@ -7,6 +7,7 @@ $arrayJSON = array();
 //Recuperiamo tramite POST i vari valori
 $length = @$_POST["length"] ?? 10;
 $start = @$_POST["start"] ?? 0; //Inizio
+//$search = $_POST["search"]["value"] ?? null;//Passa parametro di ricerca
 
 
 $a = $start;
@@ -17,17 +18,25 @@ $result1 = mysqli_query($connessione, $query1) or die("Query fallita " . mysqli_
 $row = mysqli_fetch_assoc($result1);
 $end = $row["tot"]; //La lunghezza totale
 
-$query = "SELECT * FROM employees LIMIT $a, $b";
-$result = mysqli_query($connessione, $query) or die("Query fallita " . mysqli_error($connessione) . " " . mysqli_errno($connessione));
-while ($row = mysqli_fetch_assoc($result)) {
-    $rows[] = $row;
-}
+//if(isset($search)){
+//    $query = "SELECT * FROM employees WHERE first_name == $search OR last_name == $search OR id == $search LIMIT $a, $b";
+//    $result = mysqli_query($connessione, $query) or die("Query fallita " . mysqli_error($connessione) . " " . mysqli_errno($connessione));
+//    while ($row = mysqli_fetch_assoc($result)) {
+//        $rows[] = $row;
+//    }
+//}else {
+    $query = "SELECT * FROM employees LIMIT $a, $b";
+    $result = mysqli_query($connessione, $query) or die("Query fallita " . mysqli_error($connessione) . " " . mysqli_errno($connessione));
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+//}
+
+
 $arrayJSON["data"] = $rows;
 
 $arrayJSON["recordsFiltered"] = $end;
 $arrayJSON["recordsTotal"] = $end;
 
 echo json_encode($arrayJSON);
- 
-
 ?>
