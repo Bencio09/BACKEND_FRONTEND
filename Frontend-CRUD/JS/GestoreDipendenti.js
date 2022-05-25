1e6e5fe81acd935377fab0b88228f426902f83d2
lai2-01 <lai2-01@itismeucci.com>
2022-05-25 13:40:11 +0200

    Quasi fatto

--- Frontend-CRUD/JS/GestoreDipendenti.js
+++ Frontend-CRUD/JS/GestoreDipendenti.js
@@ -1,10 +1,25 @@
-$(document).ready(function () {
-    $('#example').DataTable({
-        processing: true,
-        serverSide: true,
+
+var editor; // use a global for the submit and return data rendering in the examples
+ 
+$(document).ready(function() {
+    editor = new $.fn.dataTable.Editor( {
+        ajax: "./index.php",
+        table: "#example",
+        fields: [ {
+                label: "First name:",
+                name: "users.first_name"
+            }, {
+                label: "Last name:",
+                name: "users.last_name"
+            }
+        ]
+    } );
+ 
+    var table = $('#example').DataTable( {
+        dom: "Bfrtip",
         ajax: {
-            url: './index.php',
-            type: 'POST',
+            url: "./index.php",
+            type: 'POST'
         },
         columns: [
             { data: 'first_name' },
@@ -13,5 +28,33 @@ $(document).ready(function () {
             { data: 'hire_date' },
             { data: 'gender' },
         ],
-    });
-});
+        select: true,
+        buttons: [
+            { extend: "create", editor: editor },
+            { extend: "edit",   editor: editor },
+            {
+                extend: "selected",
+                text: 'Delete',
+                action: function ( e, dt, node, config ) {
+                    var rows = table.rows( {selected: true} ).indexes();
+ 
+                    editor
+                        .hide( editor.fields() )
+                        .one( 'close', function () {
+                            setTimeout( function () { // Wait for animation
+                                editor.show( editor.fields() );
+                            }, 500 );
+                        } )
+                        .edit( rows, {
+                            title: 'Delete',
+                            message: rows.length === 1 ?
+                                'Are you sure you wish to delete this row?' :
+                                'Are you sure you wish to delete these '+rows.length+' rows',
+                            buttons: 'Delete'
+                        } )
+                        .val( 'users.removed_date', (new Date()).toISOString().split('T')[0] );
+                }
+            }
+        ]
+    } );
+} );
