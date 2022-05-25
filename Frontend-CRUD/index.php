22a574759c16f69897a9733e67daf1f9b7bb020e
lai2-01 <lai2-01@itismeucci.com>
2022-05-25 12:07:38 +0200

    ydfyghlòà

--- Frontend-CRUD/index.php
+++ Frontend-CRUD/index.php
@@ -7,8 +7,7 @@ $arrayJSON = array();
 //Recuperiamo tramite POST i vari valori
 $length = @$_POST["length"] ?? 10;
 $start = @$_POST["start"] ?? 0; //Inizio
-//$search = $_POST["search"]["value"] ?? null;//Passa parametro di ricerca
-
+$search = @$_POST["search"]["value"] ?? null;//Passa parametro di ricerca
 
 $a = $start;
 $b = $length;
@@ -18,19 +17,29 @@ $result1 = mysqli_query($connessione, $query1) or die("Query fallita " . mysqli_
 $row = mysqli_fetch_assoc($result1);
 $end = $row["tot"]; //La lunghezza totale
 
-//if(isset($search)){
-//    $query = "SELECT * FROM employees WHERE first_name == $search OR last_name == $search OR id == $search LIMIT $a, $b";
-//    $result = mysqli_query($connessione, $query) or die("Query fallita " . mysqli_error($connessione) . " " . mysqli_errno($connessione));
-//    while ($row = mysqli_fetch_assoc($result)) {
-//        $rows[] = $row;
-//    }
-//}else {
+if(!is_null($search) && !empty($search)){
+
+    //preparo lo statement
+    $searchLike = '%'.$search.'%';
+    $stmt = mysqli_prepare($connessione, "SELECT * FROM employees WHERE first_name LIKE ? OR last_name LIKE ? OR id = ? LIMIT ?, ?");
+    /* bind parameters for markers */
+    $stmt->bind_param("sssii", $searchLike, $searchLike, $search, $a, $b);
+    
+    /* execute query */
+    $stmt->execute();
+    
+    /* fetch result */
+    $result = $stmt->get_result();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+}else {
     $query = "SELECT * FROM employees LIMIT $a, $b";
     $result = mysqli_query($connessione, $query) or die("Query fallita " . mysqli_error($connessione) . " " . mysqli_errno($connessione));
     while ($row = mysqli_fetch_assoc($result)) {
         $rows[] = $row;
     }
-//}
+}
 
 
 $arrayJSON["data"] = $rows;
